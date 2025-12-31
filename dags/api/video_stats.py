9df680fc6c2f105c5c8fe09f2a26269d2426483b
Michael Rodionov <michael.rodionov@sumsub.com>
2025-12-31 20:59:21 +0100

    added airflow support, JSON extracted and database connection

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -3,12 +3,14 @@ import os
 from dotenv import load_dotenv
 from datetime import date
 import json
+from airflow.decorators import task
+from airflow.models import Variable
 
-load_dotenv(dotenv_path="./.env")
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = 'MrBeast'
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
+@task
 def get_playlist_id():
     try:
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
@@ -20,7 +22,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def get_video_ids(playlistId):
 
     video_ids = []
@@ -49,7 +51,7 @@ def get_video_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -88,9 +90,9 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def save_to_json(extracted_data):
-    file_path = f"./data/YT_data_{date.today}.json"
+    file_path = f"./data/YT_data_{date.today()}.json"
     with open (file_path, "w", encoding="utf-8") as json_out_file:
         json.dump(extracted_data, json_out_file, indent=4, ensure_ascii=False)
 
